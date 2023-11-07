3e042e7a651da66bff84c9d31d8ead24b47fd177
Valerie Ivanova <valeri9233@gmail.com>
2023-11-08 00:29:14 +0200

    Updated the intro section

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -1,23 +1,22 @@
 export default function IntroSection() {
-    return (
-        <section id="cta" className="jumbotron text-center">
-          <div className="container">
-            <a href="food-index.html">
-              <img
-                src="images/version/food-logo.png"
-                alt=""
-                className="img-fluid"
-              />
-            </a>
-            <p>
-              Each year we've hosted the Best Food Blog Awards, we're astounded
-              at the depth, variety, creativity, and ingenuity of the food blogs
-              nominated..
-            </p>
-            <a href="#" className="btn btn-primary">
-              View Recipes
-            </a>
-          </div>
-        </section>
-    )
+  return (
+    <section id="cta" className="jumbotron text-center">
+      <div className="container">
+        <a href="food-index.html">
+          <img
+            src="images/version/food-logo.png"
+            alt=""
+            className="img-fluid"
+          />
+        </a>
+        <p>
+          Welcome to my personal Blog - a culinary journey through my passion for
+          cooking and sharing delicious recipes.
+        </p>
+        <a href="#" className="btn btn-primary">
+          View Recipes
+        </a>
+      </div>
+    </section>
+  );
 }
