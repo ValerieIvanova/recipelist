80df974dff560282ad8e3e80d1ffa1aa78dce4ea
Valerie Ivanova <valeri9233@gmail.com>
2023-11-25 13:37:17 +0200

    added more services for crud operations

--- client/src/services/RecipeService.js
+++ client/src/services/RecipeService.js
@@ -1,14 +1,42 @@
 const baseURL = "http://localhost:3030/jsonstore";
 
-export const add = async (recipeData) => {
-  const response = await fetch(`${baseURL}/recipes`, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(recipeData),
-  });
-  const result = await response.json();
+export const create = async (recipeData) => {
+    const response = await fetch(`${baseURL}/recipes`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipeData),
+    });
+    const result = await response.json();
 
-  return result;
+    return result;
+};
+
+export const getAll = async () => {
+    const response = await fetch(`${baseURL}/recipes`);
+    const result = await response.json();
+};
+
+export const getById = async (id) => {
+    const response = await fetch(`${baseURL}/recipes/${id}`);
+    const result = await response.json();
+};
+
+export const update = async (id, recipeData) => {
+    const response = await fetch(`${baseURL}/recipes/${id}`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipeData),
+    });
+    const result = await response.json();
+};
+
+export const deleteById = async (id) => {
+    const response = await fetch(`${baseURL}/recipes/${id}`, {
+        method: "delete",
+    });
+    const result = await response.json();
 };
