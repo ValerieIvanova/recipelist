b7f110324c2b30f31fc1ed7cedb7753b9426a24e
Valerie Ivanova <valeri9233@gmail.com>
2023-11-27 00:03:03 +0200

    moved the comments section in a new component

---
+++ client/src/components/detailsRecipe/comments/Comments.jsx
@@ -0,0 +1,104 @@
+export default function Comments() {
+    return (
+        <>
+            <div className="custombox clearfix">
+                <h4 className="small-title">3 Comments</h4>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="comments-list">
+                            <div className="media">
+                                <a className="media-left" href="#">
+                                    <img
+                                        src="upload/author.jpg"
+                                        alt=""
+                                        className="rounded-circle"
+                                    />
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading user_name">
+                                        Amanda Martines{" "}
+                                        <small>5 days ago</small>
+                                    </h4>
+                                    <p>
+                                        Exercitation photo booth stumptown tote
+                                        bag Banksy, elit small batch freegan
+                                        sed. Craft beer elit seitan
+                                        exercitation, photo booth et 8-bit kale
+                                        chips proident chillwave deep v laborum.
+                                        Aliquip veniam delectus, Marfa eiusmod
+                                        Pinterest in do umami readymade swag.
+                                        Selfies iPhone Kickstarter, drinking
+                                        vinegar jean.
+                                    </p>
+                                    <a
+                                        href="#"
+                                        className="btn btn-primary btn-sm"
+                                    >
+                                        Reply
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="media">
+                                <a className="media-left" href="#">
+                                    <img
+                                        src="upload/author_01.jpg"
+                                        alt=""
+                                        className="rounded-circle"
+                                    />
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading user_name">
+                                        Baltej Singh <small>5 days ago</small>
+                                    </h4>
+                                    <p>
+                                        Drinking vinegar stumptown yr pop-up
+                                        artisan sunt. Deep v cliche lomo
+                                        biodiesel Neutra selfies. Shorts fixie
+                                        consequat flexitarian four loko tempor
+                                        duis single-origin coffee. Banksy, elit
+                                        small.
+                                    </p>
+                                    <a
+                                        href="#"
+                                        className="btn btn-primary btn-sm"
+                                    >
+                                        Reply
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="media last-child">
+                                <a className="media-left" href="#">
+                                    <img
+                                        src="upload/author_02.jpg"
+                                        alt=""
+                                        className="rounded-circle"
+                                    />
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading user_name">
+                                        Marie Johnson <small>5 days ago</small>
+                                    </h4>
+                                    <p>
+                                        Kickstarter seitan retro. Drinking
+                                        vinegar stumptown yr pop-up artisan
+                                        sunt. Deep v cliche lomo biodiesel
+                                        Neutra selfies. Shorts fixie consequat
+                                        flexitarian four loko tempor duis
+                                        single-origin coffee. Banksy, elit
+                                        small.
+                                    </p>
+                                    <a
+                                        href="#"
+                                        className="btn btn-primary btn-sm"
+                                    >
+                                        Reply
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
