f80c8e3f33ac8844304733de29f0ba8b9bc1601e
Valerie Ivanova <valeri9233@gmail.com>
2023-11-26 23:19:06 +0200

    fixed the paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,10 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-import ContactMe from "./components/ContactMe/ContactMe";
-import SingleRecipeDetails from "./components/SingleRecipeDetails/SingleRecipeDetails";
+import ContactMe from "./components/contactMe/ContactMe";
+import DetailsRecipe from "./components/detailsRecipe/DetailsRecipe";
 import CreateRecipe from "./components/CreateRecipe/CreateRecipe";
-import AllRecipes from "./components/allRecipes/AllRecipes";
+import AllRecipes from "./components/AllRecipes/AllRecipes"
 
 function App() {
   return (
@@ -16,9 +16,11 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> */}
         <Route path="/recipes" element={<AllRecipes />} />
-        <Route path="/recipe1" element={<SingleRecipeDetails />} />
         <Route path="/recipes/create" element={<CreateRecipe />} />
+        <Route path="/recipes/:recipeId" element={<DetailsRecipe />} />
         <Route path="/contacts" element={<ContactMe />} />
       </Routes>
 
