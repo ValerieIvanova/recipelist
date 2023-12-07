d9e66b676a676542d0ff60fc8e7f2594fc773d5f
Valerie Ivanova <valeri9233@gmail.com>
2023-12-07 22:25:38 +0200

    added popular categories logic

--- client/src/components/asideBar/AsideBar.jsx
+++ client/src/components/asideBar/AsideBar.jsx
@@ -6,10 +6,29 @@ import RecentPostsItem from "./recentPostsItem.jsx/RecentPostsItem";
 
 export default function AsideBar() {
     const [recipes, setRecipes] = useState([]);
+    const [categoriesLength, setCategoriesLength] = useState([]);
 
     useEffect(() => {
         recipeService.getLatestFive().then((result) => setRecipes(result));
     }, []);
+
+    useEffect(() => {
+        recipeService
+            .getByPopularity()
+            .then((result) => {
+                const categories = result.map((recipe) => recipe.category);
+                const uniqueCategories = [...new Set(categories)];
+                const categoriesLength = uniqueCategories.map((category) => {
+                    return {
+                        categoryName: category,
+                        count: result.filter((recipe) => recipe.category === category).length,
+                    };
+                });
+
+                setCategoriesLength(categoriesLength);
+            })
+            .catch((error) => console.log(error));
+    });
 
     return (
         <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12">
@@ -30,14 +49,9 @@ export default function AsideBar() {
                     <h2 className="widget-title">Recent Posts</h2>
                     <div className="blog-list-widget">
                         <div className="list-group">
-
                             {recipes.map((recipe) => (
-                                <RecentPostsItem
-                                    key={recipe._id}
-                                    {...recipe}
-                                />
+                                <RecentPostsItem key={recipe._id} {...recipe} />
                             ))}
-
                         </div>
                     </div>
                 </div>
@@ -46,41 +60,13 @@ export default function AsideBar() {
                     <h2 className="widget-title">Popular Categories</h2>
                     <div className="link-widget">
                         <ul>
-                            <li>
-                                <a href="#">
-                                    Fahsion <span>(21)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Lifestyle <span>(15)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Art & Design <span>(31)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Health Beauty <span>(22)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Clothing <span>(66)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Entertaintment <span>(11)</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Food & Drink <span>(87)</span>
-                                </a>
-                            </li>
+                            {categoriesLength.map(({ categoryName, count }) => (
+                                <li key={categoryName}>
+                                    <a href="">{categoryName} 
+                                    <span>{count}</span>
+                                    </a>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
